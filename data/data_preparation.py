3e83d0dc8d613f1b1079f82f5653ae0e271b5b23
Maciej Grzeszczak <maciejgrzeszczak@gmail.com>
2018-04-05 16:34:34 +0200

    Data preparation with validation split

--- data/data_preparation.py
+++ data/data_preparation.py
@@ -1,5 +1,7 @@
 import re
+
 import unidecode
+
 
 def remove_repeating_spaces(sentence):
     return re.sub('[ ]{2,}', ' ', sentence)
@@ -23,25 +25,41 @@ def prepare_sentence(sentence):
     return prepared, len(tokenized)
 
 
-
-
-def create_prepared_data(src_sentences_file, dst_sentences_file, prepared_src_file, prepared_dst_file, size=2000000, min_length=1, max_length=40, skip=0):
+def create_prepared_data(src_sentences_file,
+                         dst_sentences_file,
+                         prepared_src_file,
+                         prepared_dst_file,
+                         validation_src_file,
+                         validation_dst_file,
+                         size=None,
+                         validation_size=0,
+                         min_length=1,
+                         max_length=40):
     with open(src_sentences_file) as source_file:
         with open(dst_sentences_file) as dst_file:
             with open(prepared_src_file, 'w') as output_src_file:
                 with open(prepared_dst_file, 'w') as output_dst_file:
-                    total = 0
-                    while total < size:
-                        source_sentence = next(source_file)
-                        dst_sentence = next(dst_file)
+                    with open(validation_src_file, 'w') as output_val_src_file:
+                        with open(validation_dst_file, 'w') as output_val_dst_file:
+                            val_total = 0
+                            total = 0
+                            while size is None or total < size:
+                                try:
+                                    source_sentence = next(source_file)
+                                    dst_sentence = next(dst_file)
+                                except:
+                                    return
 
-                        source_sentence, src_len = prepare_sentence(source_sentence)
-                        dst_sentence, dst_len = prepare_sentence(dst_sentence)
-                        if src_len > max_length or src_len < min_length or dst_len > max_length or dst_len < min_length:
-                            continue
-                        if skip > 0:
-                            skip -= 1
-                            continue
-                        total += 1
-                        output_src_file.write(source_sentence + '\n')
-                        output_dst_file.write(dst_sentence + '\n')
+                                source_sentence, src_len = prepare_sentence(source_sentence)
+                                dst_sentence, dst_len = prepare_sentence(dst_sentence)
+                                if src_len > max_length or src_len < min_length or dst_len > max_length or dst_len < min_length:
+                                    continue
+
+                                if val_total < validation_size:
+                                    val_total += 1
+                                    output_val_src_file.write(source_sentence + '\n')
+                                    output_val_dst_file.write(dst_sentence + '\n')
+                                else:
+                                    total += 1
+                                    output_src_file.write(source_sentence + '\n')
+                                    output_dst_file.write(dst_sentence + '\n')
